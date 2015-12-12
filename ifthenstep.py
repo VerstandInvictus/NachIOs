c17177661bc73058f7c8cbec64f72f9febc6f311
Peter K. Cawley <peter.k.cawley@gmail.com>
2015-12-12 10:16:57 -0800

    root node can now be passed as a param

--- ifthenstep.py
+++ ifthenstep.py
@@ -1,6 +1,13 @@
 # IFTTT email to Hook.io Nach step creator.
 #
-# Parameters are ?sec=<password>&sub=<subject>&bo=<body>&att=<attachmenturl>.
+# Parameters are:
+# ?sec=<password>
+# &sub=<subject>
+# &bo=<body>
+# &att=<attachmenturl>
+# &par=<parent node>
+# All except sec are optional if properly configured, except 
+# there must be either a subject or a body or both.
 #
 # This adds a step to a preconfigured Nach inbox node with sub as the name
 # and att and body as a note if present.
@@ -9,6 +16,10 @@
 # Email trigger; in particular, attachmenturl implements IFTTT's feature
 # where if you email them an attachment, it becomes a public URL hosted on
 # their server to allow a sort of hacky way to send arbitrary files to Nach.
+#
+# The killer app here is using this with a shortcut Intent on android
+# via something like Email to Me so that you can share anything to Nach
+# with two taps.
 #
 # It is not authenticated because IFTTT doesn't really support HTTP auth;
 # as a workaround it uses a secret word stored in Hook
@@ -21,7 +32,7 @@ import json
 
 # begin setting vars. This should be your username
 user = Hook['env']['nachuser']
-# this is set to my inbox's node ID but it could be anything
+# default value - this is set to my inbox's node ID but it could be anything
 parentNode = Hook['env']['nachsteproot']
 # to avoid publicizing API key, store it in your Hook env vars (hook.io/env).
 apiKey = Hook['env']['nachkey']
@@ -32,6 +43,10 @@ magicWord = Hook['env']['magicword']
 # now we set the parameters
 secret = Hook['params']['sec']
 # catch empty params
+try:
+    parentNode = Hook['params']['par']
+except KeyError:
+    pass
 try:
     subject = Hook['params']['sub']
 except KeyError:
@@ -45,6 +60,13 @@ try:
 except KeyError:
     note = None
 
+# I use the below signature to keep Gmail on Android from prompting me
+# about empty body in an email. This could be anything you want. Strip it out:
+signature = "\n\n______\n"
+if body.endswith(signature):
+    cut = len(signature)
+    body = body[:-cut]
+
 #strip blank params passed from IFTT/placeholders
 toss = ("", '\n', "NOSUB")
 if note in toss:
@@ -53,13 +75,6 @@ if body in toss:
     note = None
 if len(body) < 3:
     body = None
-
-# I use the below signature to keep Gmail on android from prompting me
-# about empty body in an email. This could be anything you want. Strip it out:
-signature = "\n\n______\n"
-if body.endswith(signature):
-    cut = len(signature)
-    body = body[:-cut]
     
 # if no subject (read: shared from Google Keep), use body as subject
 # but keep body as note in case of errors/truncation
@@ -82,7 +97,6 @@ if magicWord == secret:
         "type": "Step",
         "name": subject
     })
-    print newStep.text
     # get the ID of the new step and add notes
     response = json.loads(newStep.text)
     nodeId = response['id']
@@ -92,5 +106,6 @@ if magicWord == secret:
         bodyNote = postNote(nodeId, body, apiKey)
 
 # nedry.py
+# unfortunately Hook doesn't let python access logs yet
 else:
     print "Ah ah ah! You didn't say the magic word!"
