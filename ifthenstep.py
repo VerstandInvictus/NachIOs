b2adfbf8a7f3db6be8225d3c6e00f3b8b4d23cd6
Peter K. Cawley <peter.k.cawley@gmail.com>
2015-12-13 18:30:03 -0800

    custom category shortcuts

--- ifthenstep.py
+++ ifthenstep.py
@@ -6,22 +6,23 @@
 # &msg=_B_R_K_{{Subject}}_B_R_K_{{Body}}_B_R_K_{{AttachmentUrl}}
 #
 # The MSG parameter can contain as many items as needed and they'll all
-# be added as notes.
+# be added as notes provided they are separated by _B_R_K_. This is
+# accomplished by parsing the URL IFTTT requests directly, not via the param.
 # 
 # It is done in this strange way because IFTTT doesn't escape URLs within
-# its URLs.
+# its URLs. This causes Hook's params to break on restricted chars (read: &).
 #
-# This adds a step to a preconfigured Nach inbox node with sub as the name
-# and att and body as a note if present.
+# This adds a step to a preconfigured Nach inbox node with the first nonblank
+# BRK param as the subject and all others as notes.
 #
 # It is intended to be used with IFTTT's Maker channel action and
-# Email trigger; in particular, attachmenturl implements IFTTT's feature
+# Email trigger; in particular, with IFTTT's feature
 # where if you email them an attachment, it becomes a public URL hosted on
 # their server to allow a sort of hacky way to send arbitrary files to Nach.
 #
 # The killer app here is using this with a shortcut Intent on android
 # via something like Email to Me so that you can share anything to Nach
-# with two taps.
+# with two taps. I also use it for Pocket -> IFTTT -> Hook -> Nach.
 #
 # It is not authenticated because IFTTT doesn't really support HTTP auth;
 # as a workaround it uses a secret word stored in Hook
@@ -51,6 +52,7 @@ try:
 except KeyError:
     pass
 
+# this is where we get the URL and split it to a list of pseudo-params
 msgstring = urllib.unquote(str(Hook['req']['url'])).split("_B_R_K_")
 
 subject = msgstring[1]
@@ -58,8 +60,7 @@ if len(msgstring) > 2:
     notes = msgstring[2:]
 
 # I use the below signature to keep Gmail on Android from prompting me
-# about empty body in an email. This could be anything you want. Strip it out:
-
+# about empty body in an email. This could be anything you want. Strip it out:  
 newNotes = list()
 signature = "\n\n______\n"
 for each in notes:
@@ -78,6 +79,27 @@ notes=filteredNotes
 # but keep body as note in case of errors/truncation
 if subject in toss:
     subject = notes[0]
+
+# custom categorization shortcuts for some of my goals
+# replace or delete these with yours
+if "music" in subject.lower():
+	parentNode = 47745
+if "wp" in subject.lower():
+	parentNode = 49021
+if "[dot]" in subject.lower():
+	parentNode = 47587
+if "deviantart" in subject.lower():
+	parentNode = 47688
+if "buy" in subject.lower():
+	parentNode = 47577
+if "watch" in subject.lower():
+    parentNode = 47553
+if "book" in subject.lower() or "calibre" in subject.lower():
+	parentNode = 47555
+if "pay" in subject.lower():
+	parentNode = 47557
+if "work" in subject.lower():
+	parentNode = 47541
 
 # DRY note posting func
 def postNote(node, content, apikey):
