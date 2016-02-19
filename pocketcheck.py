a1b31f5939389038f900cc2cf84050aac120d0c7
VerstandInvictus <peter.k.cawley@gmail.com>
2016-02-19 09:41:14 -0800

    time threshold stored in mongo

--- pocketcheck.py
+++ pocketcheck.py
@@ -1,10 +1,23 @@
 import pocket
 import config
-import pprint
+import time
+import pymongo
+
+client = pymongo.MongoClient()
+db = client.nachios
+timedb = db.time
+
+# this code for init-ing blank DB
+# timedb.insert_one(dict(
+#     _id="lastTime",
+#     lastTime=time.time()
+# ))
+
+lastTime = timedb.find_one({"_id": "lastTime"})
 
 p = pocket.Pocket(config.consumerKey, config.accessToken)
 
-articleList = p.get(detailType="simple", state="archive")
+articleList = p.get(detailType="simple", state="archive", since=lastTime)
 
 for each in articleList[0]['list'].itervalues():
     buildup = ' '
